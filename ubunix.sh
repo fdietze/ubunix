5e9dd2cf947888219d1e57a53155e7b5f6fa556f
Felix Dietze <felix.dietze@rwth-aachen.de>
2020-10-19 18:29:19 +0100

    Add install aliases

--- ubunix.sh
+++ ubunix.sh
@@ -1,8 +1,17 @@
+IMAGEPATH=$HOME/projects/ubunix
 
 dr() {                                                                                               I 
-    docker build $HOME/projects/ubunix -t ubunix
+    docker build $IMAGEPATH -t ubunix
     docker run -t -i \
         --mount type=bind,source="$HOME",target="$HOME" \
         --mount type=bind,source="$(pwd)",target="$(pwd)" \
             ubunix bash -c "useradd --uid $UID --gid $GID $USER && cd $(pwd) && su $USER -c '$(echo $@)'"; 
+}
+
+dr-add-run() {
+    echo "RUN $@" >> $IMAGEPATH/Dockerfile
+}
+
+dr-install() {
+    echo "RUN apt-get -y install $@" >> $IMAGEPATH/Dockerfile
 }
