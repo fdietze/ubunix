d298e7781779867251edab50a2974b06acaa9dc0
Felix Dietze <felix.dietze@rwth-aachen.de>
2020-11-09 22:08:59 +0000

    Fix terminal colors

--- ubunix.sh
+++ ubunix.sh
@@ -5,6 +5,7 @@ IMAGEPATH=$HOME/projects/ubunix
 dr() {                                                                                               I 
     docker build "$IMAGEPATH" --quiet --tag ubunix && \
     docker run -t -i \
+        -e "TERM=xterm-256color" \
         --mount type=bind,source="$HOME",target="$HOME" \
         --mount type=bind,source="$(pwd)",target="$(pwd)" \
             ubunix bash -c "useradd --uid $UID --gid $GID $USER && cd $(pwd) && su $USER -c '$*'"; 
