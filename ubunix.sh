0da339cce01f1616de4088688a7c57fd3b6f77dc
Felix Dietze <felix.dietze@rwth-aachen.de>
2021-01-04 18:09:35 +0100

    expose tmp

--- ubunix.sh
+++ ubunix.sh
@@ -4,7 +4,9 @@ IMAGEPATH=$HOME/projects/ubunix
 
 # https://medium.com/@SaravSun/running-gui-applications-inside-docker-containers-83d65c0db110
 # https://github.com/mviereck/x11docker/wiki/Hardware-acceleration
+# https://docs.docker.com/engine/reference/commandline/run/#access-an-nvidia-gpu
 # https://stackoverflow.com/questions/28985714/run-apps-using-audio-in-a-docker-container/28985715#28985715
+# https://github.com/mviereck/x11docker
 
 dr() {
     # https://stackoverflow.com/questions/1668649/how-to-keep-quotes-in-bash-arguments/8723305#8723305
@@ -19,6 +21,7 @@ dr() {
         --env "UBUNIX=true" \
         --env="DISPLAY" \
         --env="XDG_RUNTIME_DIR" \
+        --env="TMP=/tmp" \
         --net=host \
         --device /dev/fuse --cap-add SYS_ADMIN \
         --ipc=host \
@@ -29,6 +32,7 @@ dr() {
         -v /etc/machine-id:/etc/machine-id \
         -v /run/user/$UID:/run/user/$UID \
         -v /var/lib/dbus:/var/lib/dbus \
+        -v /tmp:/tmp \
         --mount type=bind,source="$HOME",target="$HOME" \
         --mount type=bind,source="$(pwd)",target="$(pwd)" \
             ubunix bash -c "useradd --uid $UID --gid $GID $USER && cd $(pwd) && echo -e 'root ALL=(ALL:ALL) SETENV: ALL\n $USER	ALL=(ALL:ALL)	NOPASSWD:SETENV: ALL' >> /etc/sudoers && su $USER --session-command '$(basename "$SHELL") -ic \"$COMMAND\"'"; 
